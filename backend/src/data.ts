9425736669cd024936ef149694c1f09126654b60
ebinmoolan@gmail.com <bennye@tcd.ie>
2018-11-28 12:57:33 +0000

    Added interface for userRepos model

--- backend/src/data.ts
+++ backend/src/data.ts
@@ -1,9 +1,15 @@
 import { Document, Model, model, Schema } from 'mongoose';
 
+export interface IUserRepos extends Document {
+    repoName: string;
+    repoSize: number;
+    userID: number;
+}
+
 const userRepos = new Schema({
     repoName: String,
     repoSize: Number,
     userId: Number,
 });
 
-export const UserRepos = model('UserRepos', userRepos);
+export const UserRepos: Model<IUserRepos> = model<IUserRepos>('UserRepos', userRepos);
