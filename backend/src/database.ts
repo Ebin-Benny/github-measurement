d258796a124bf27e697d3289d7f2244e3ea933d8
ebinmoolan@gmail.com <bennye@tcd.ie>
2018-12-01 20:39:05 +0000

    Added call to error() when promises are rejected

--- backend/src/database.ts
+++ backend/src/database.ts
@@ -2,77 +2,85 @@ import { RepoContributions, UserRepos } from './data';
 import { getRepoContributions as getContributions, getUserRepos as getUsers } from './requests';
 
 export const getUserRepos = async (userName: string, callback: any, error: any) => {
-  let ret = await UserRepos.findOne({ name: userName });
-
-  if (!ret) {
-    const data = new UserRepos();
-    const userRepos = await getUsers(userName);
-
-    data.name = userName;
-
-    let index = 0;
-    for (const repo of userRepos.children) {
-      if (repo.language === undefined) {
-        repo.language = 'N/A';
-      }
-      data.children[index] = {
-        id: repo.id,
-        language: repo.language,
-        name: repo.name,
-        size: repo.size,
-      };
-      index++;
-    }
-
-    await data.save();
-
-    ret = await UserRepos.findOne({ name: userName });
+  try {
+    let ret = await UserRepos.findOne({ name: userName });
 
     if (!ret) {
-      error();
+      const data = new UserRepos();
+      const userRepos = await getUsers(userName);
+
+      data.name = userName;
+
+      let index = 0;
+      for (const repo of userRepos.children) {
+        if (repo.language === undefined) {
+          repo.language = 'N/A';
+        }
+        data.children[index] = {
+          id: repo.id,
+          language: repo.language,
+          name: repo.name,
+          size: repo.size,
+        };
+        index++;
+      }
+
+      await data.save();
+
+      ret = await UserRepos.findOne({ name: userName });
+
+      if (!ret) {
+        error();
+      } else {
+        callback(ret);
+      }
     } else {
       callback(ret);
     }
-  } else {
-    callback(ret);
+  } catch (e) {
+    error();
   }
 };
 
 export const getRepoContributions = async (owner: string, repo: string, callback: any, error: any) => {
-  let ret = await RepoContributions.findOne({ name: owner + '/' + repo });
-
-  if (!ret) {
-    const data = new RepoContributions();
-    const repoContributions = await getContributions(owner, repo);
-
-    data.name = repoContributions.name;
-
-    let index = 0;
-    for (const week of repoContributions.weeks) {
-      let statIndex = 0;
-      data.weeks[index] = { week: week.week, stats: [] };
-      for (const stat of week.stats) {
-        data.weeks[index].stats[statIndex++] = {
-          additions: stat.additions,
-          author: stat.author,
-          commits: stat.commits,
-          deletions: stat.deletions,
-          net: stat.net,
-        };
-      }
-      index++;
-    }
-
-    await data.save();
-
-    ret = await RepoContributions.findOne({ name: owner + '/' + repo });
+  try {
+    let ret = await RepoContributions.findOne({ name: owner + '/' + repo });
 
     if (!ret) {
-      error();
+      const data = new RepoContributions();
+      const repoContributions = await getContributions(owner, repo);
+
+      data.name = repoContributions.name;
+
+      let index = 0;
+      for (const week of repoContributions.weeks) {
+        let statIndex = 0;
+        data.weeks[index] = { week: week.week, stats: [] };
+        for (const stat of week.stats) {
+          data.weeks[index].stats[statIndex++] = {
+            additions: stat.additions,
+            author: stat.author,
+            commits: stat.commits,
+            deletions: stat.deletions,
+            net: stat.net,
+          };
+        }
+        index++;
+      }
+
+      await data.save();
+
+      ret = await RepoContributions.findOne({ name: owner + '/' + repo });
+
+      if (!ret) {
+        error();
+      } else {
+        callback(ret);
+      }
     } else {
       callback(ret);
     }
-  } else {
-    callback(ret);
+  } catch (e) {
+    error();
   }
 };
